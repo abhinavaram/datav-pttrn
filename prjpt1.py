c43f9c5c33124026e5b9260ff7eb7e476d4bd013
abhinavaram <136364820+abhinavaram@users.noreply.github.com>
2023-06-25 14:47:18 +0530

    Update prjpt1.py

--- prjpt1.py
+++ prjpt1.py
@@ -40,16 +40,44 @@ while True:
         break
 
 if len(charts) > 0:
+    colors = ['blue', 'green', 'red', 'orange', 'purple', 'brown']
+    highlighted_colors = []
+
     fig, axes = plt.subplots(len(charts), 1, figsize=(10, 6 * len(charts)))
 
     for i, (x_columns, y_columns) in enumerate(charts):
         ax = axes[i] if len(charts) > 1 else axes
-        for y_column in y_columns:
-            ax.plot(df[x_columns[0]], df[y_column], marker='o')
+
+        first_y_color = colors[i % len(colors)]
+        second_y_color = colors[(i + 1) % len(colors)]
+
+        for j, y_column in enumerate(y_columns):
+            if j == 0:
+                ax.plot(df[x_columns[0]], df[y_column], color=first_y_color, marker='o')
+            elif j == 1:
+                ax.plot(df[x_columns[0]], df[y_column], color=second_y_color, marker='o')
+
+        highlight_colors = list(set(colors) - set(highlighted_colors))
+        highlighted_colors.append(highlight_colors[0])
+
+        while True:
+            start_value = float(input("Enter the starting x-axis value (or 0 to finish): "))
+            if start_value == 0:
+                break
+
+            end_value = float(input("Enter the ending x-axis value: "))
+
+            start_index = df[df[x_columns[0]] == start_value].index[0]
+            end_index = df[df[x_columns[0]] == end_value].index[0]
+
+            # Plot the highlighted part with the specified color
+            ax.plot(df[x_columns[0]][start_index:end_index + 1], df[y_columns[0]][start_index:end_index + 1], color=highlight_colors[0], marker='o')
+
         ax.set_title('Line Chart')
         ax.set_xlabel('X Values')
         ax.set_ylabel('Y Values')
 
     plt.tight_layout()
     plt.show()
+
 
